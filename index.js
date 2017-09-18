490dc9de4b189347d3d77bb407c5836f6d1f3910
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-18 05:31:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var holiday = "New Years"
 var days = "20"
 
 function happyHolidays() {
-  return "Happy Holidays!"
+  return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday,name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days,holiday) {
+function holidayCountdown(holiday,days) {
   return `It's ${days} days until ${holiday}!`
 }
